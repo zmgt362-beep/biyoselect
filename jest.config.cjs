e824d8e5a2afb9a450f7b80a1526f6400b5b5323
zmgt362-beep <zmgt362@gmail.com>
2026-09-14 23:58:18 +0900

    test: configure Jest for TypeScript recommendation tests

---
+++ jest.config.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.tsx?$": ["ts-jest", { tsconfig: "tsconfig.json" }],
+  },
+  testPathIgnorePatterns: ["/.next/", "/node_modules/"],
+};
